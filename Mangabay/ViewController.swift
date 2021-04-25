013d0116a5bb668c2553957c333cc7a339206ec6
tmnabeel30 <tmnabeel30@gmail.com>
2021-04-25 08:34:53 +0530

    Initial Commit

---
+++ Mangabay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mangabay
+//
+//  Created by Nabeel on 4/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
